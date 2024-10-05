c2c7fbc0e0ebfe142c5f5a679be7f35b5974ea37
Alessandro Farcas <alessandro@alenygam.com>
2024-10-05 17:30:03 +0200

    Suss ඞ

---
+++ ml/include/k_means.h
@@ -0,0 +1,13 @@
+#ifndef K_MEANS_H
+#define K_MEANS_H
+
+#include <vector>
+
+struct trained_k_means_algo_t {
+    std::vector<long double> centr1;
+    std::vector<long double> centr2;
+};
+
+trained_k_means_algo_t train_k_means(std::vector<long double> &seed1, std::vector<long double> &seed2);
+
+#endif
