e01e51c3ea2966932e72008032c632d3104bd3ef
pandaa73 <nicholas.bamber73@gmail.com>
2024-10-05 16:03:36 +0200

    Added signature for pre-calc function

---
+++ include/defs.h
@@ -0,0 +1,6 @@
+#ifndef __SAC24_DEFS_H
+#define __SAC24_DEFS_H
+
+using ld = long double;
+
+#endif /* __SAC24_DEFS_H */
---
+++ pre/src/pre.cpp
@@ -0,0 +1,11 @@
+#include <pre.h>
+
+#include <cstddef>
+
+void parse12(std::vector<data_point_t> &out) {
+    static std::size_t it = 0;
+
+    /* implementa */
+
+    it++;
+}
