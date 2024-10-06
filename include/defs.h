6bc5b06d19d68fa642e6558bf63d749561914731
pandaa73 <nicholas.bamber73@gmail.com>
2024-10-06 12:33:22 +0200

    Train FFT Testing Ready

--- include/defs.h
+++ include/defs.h
@@ -5,20 +5,10 @@
 
 using ld = long double;
 
-constexpr std::size_t DATAPOINTS_PER_12H = 4320;
 constexpr std::size_t FFT_OUT_APPROX = 32;
-constexpr ld SAMPLE_RATE = 0.1l;
-
-
-// 8 data blocks implies that each block
-// will have 24/8 = 3 hours of data
-constexpr std::size_t DATA_BLOCKS = 8;
-
-// Same as SAMPLE_RATE
-constexpr ld DATA_POINT_FREQ = 1.0l;
-
-// Same as DATAPOINTS_PER_12H
 constexpr std::size_t DATA_POINT_PER_BLOCK = 10800;
+constexpr std::size_t DATA_BLOCKS = 8;
+constexpr ld DATA_POINT_FREQ = 1.0l;
 
 typedef struct {
     float time;
